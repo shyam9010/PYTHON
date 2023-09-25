28a108378ae7936a425a9a5fadd5fb615979b6a9
shyam9010 <shyamsundar9010@gmail.com>
2023-09-25 21:14:00 +0530

    operators

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
-print('hello world')
-print('hi welocome')
+a = 177
+b = 20
+c = a + b 
+d = a - b
+e = a * b
+f = a % b
+print(f)
+g = a ** b
+h = a // b 
+e = a / b
+print(c, d, e, f, g, h, e)
---
+++ logical.py
@@ -0,0 +1,12 @@
+a= 15
+b = 10
+
+print (a > b and b < 12 )
+# check if both statesments are correct or not
+
+print (a < b or b < 20 )
+# check if either one  are correct 
+
+print ( not(a<b))
+
+# check if statement is not correct 
