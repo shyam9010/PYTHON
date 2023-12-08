c8de51187bbb1e36a8f633b4d62b6732dfd3e0be
shyam9010 <43133319+shyam9010@users.noreply.github.com>
2023-12-08 09:47:41 +0530

    Update evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -1,10 +1,12 @@
-number1 = int(input("Enter a number : "))
+def even():
+    number1 = int(input("Enter a number : "))
 
-while True:
-    if number1 % 2 == 0:
-        print(f"The number {number1} is even")
-        b = int(input("enter a number"))
-        if b % 2 == 0:
-            break
-    else:
-        print(f"The number {number1} is odd")
+    while True:
+        if number1 % 2 == 0:
+            print(f"The number {number1} is even")
+            b = int(input("enter a number"))
+            if b % 2 == 0:
+                break
+        else:
+            print(f"The number {number1} is odd")
+even()
