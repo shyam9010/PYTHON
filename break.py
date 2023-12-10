bce8c6b40cdafbcdc0e3bf9afe7d747a1357d6ae
shyam9010 <shyamsundar9010@gmail.com>
2023-12-10 14:54:16 +0530

    break and continue

--- break.py
+++ break.py
@@ -1,7 +1,40 @@
-a = int(input(""))
+def multi():
+    number = int(input("Enter the starting number : "))
+    end = int(input("Enter the ending number : "))
+    while number <= end:
+        if number % 5 != 0:
+            print(number)
+        number += 1
 
-for i in range(1, a):
-    if i % 5 != 0:
+
+multi()
+
+
+def fun2():
+    number = int(input("Enter the starting number : "))
+    end = int(input("Enter the ending number : "))
+    while True:
+        if number > end:
+            break
+
+        if number % 5 == 0:
+            number += 1
+            continue
+        print(number)
+        number = number+1
+
+
+def fun3():
+    i = 0
+    end = int(input("Enter the end:"))
+    while True:
+        i = i + 1
+        if i > end:
+            break
+
+        if i % 5 == 0:
+            continue
         print(i)
-    else:
-        continue
+
+
+fun3()
