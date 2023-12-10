e7dbba704dc57f454af41acc26f0f08afe76290c
shyam9010 <shyamsundar9010@gmail.com>
2023-12-10 14:55:31 +0530

    programs

--- comparision.py
+++ comparision.py
@@ -1,25 +1,25 @@
-a=10
-b=20
+a = 10
+b = 20
 
 print(a == 10)
-#check if a is equal to 10
+# check if a is equal to 10
 
 print(a != 10)
 
 # check if a is not equl to 10 or not
 
-print (a > b )
+print(a > b)
 
-#check if a is greater than b or not
+# check if a is greater than b or not
 
 print(a < b)
 
 # check whether a is less than b or not
 
-print (a >= b)
+print(a >= b)
 
 # check if a is greater than or equl to b
 
-print (a <= b)
+print(a <= b)
 
 # check if a is less than or equl to b
