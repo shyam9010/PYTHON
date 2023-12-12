3055cd347ff22aedaded5ddb317e958697adcb64
shyam9010 <shyamsundar9010@gmail.com>
2023-12-12 21:51:03 +0530

    list appending using for loop

--- input.py
+++ input.py
@@ -1,6 +1,6 @@
 no_of_inputs = int(input(""))
 list_a = []
-for i in range (no_of_inputs):
-    b=int(input(""))
+for i in range(no_of_inputs):
+    b = int(input(""))
     list_a.append(b)
 print(list_a)
