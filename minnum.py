eec94f9463191b7c8182bd20517b26edc5ad4d51
shyam9010 <43133319+shyam9010@users.noreply.github.com>
2024-04-28 02:06:56 +0530

    Delete minnum.py

--- minnum.py
+++
@@ -1,9 +0,0 @@
-no_of_inputs = int(input("enter no of inputs"))
-list_a =[]
-for i in range(no_of_inputs):
-    b=int(input("Enter the inputs"))
-    list_a.append(b)
-min_num = int(input("min_num = "))
-for i in list_a:
-    if min_num<i:
-        print(i)
