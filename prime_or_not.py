2d291fcd0127649de6403564da4eba6ad3bbac34
shyam9010 <shyamsundar9010@gmail.com>
2024-04-28 22:48:02 +0530

    check the prime number in the range

--- prime_or_not.py
+++ prime_or_not.py
@@ -1,13 +1,16 @@
-num = int(input("Enter the desired number: "))
-count = 0
-if num > 1:
-    for i in range(1, num+1):
-        if (num % i) == 0:
-            count = count + 1
-else:
-    print("The number is not a prime number")
+# num = int(input("Enter the desired number: "))
+b = int(input(""))
 
-if count == 2:
-    print(num, "is a prime number")
-else:
-    print(num, "is not a prime number")
+for num in range(1, b+1):
+    count = 0
+    if num > 1:
+        for i in range(1, num+1):
+            if (num % i) == 0:
+                count = count + 1
+    else:
+        print("The number is not a prime number")
+
+    if count == 2:
+        print(num, "is a prime number")
+    else:
+        print(num, "is not a prime number")
