56c699344ee04191da50e72ecdd76e4954741c71
shyam9010 <shyamsundar9010@gmail.com>
2024-04-28 22:12:34 +0530

    prime or not

--- prime_or_not.py
+++ prime_or_not.py
@@ -1,9 +1,13 @@
 num = int(input("Enter the desired number: "))
+count = 0
 if num > 1:
-    for i in range(2, num):
+    for i in range(1, num+1):
         if (num % i) == 0:
-            print("The provided number is not a prime number")
-        else:
-            print("The number is a prime number")
+            count = count + 1
 else:
     print("The number is not a prime number")
+
+if count == 2:
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
