2732a223fc502234e3f8a54151529d1a14d43417
shyam9010 <shyamsundar9010@gmail.com>
2024-05-01 00:39:05 +0530

    two appraches for a palindrome

--- palindrome.py
+++ palindrome.py
@@ -11,3 +11,15 @@ if list == list_2:
     print(name, " is a palindrome")
 else:
     print(name, " is not a palindrome")
+# second approach
+
+c = ["s", "a", "a"]
+d = 0
+e = len(c) - 1
+while (d < e):
+    if c[d] == c[e]:
+        d = d + 1
+        e = e - 1
+    else:
+        print("not a palindrome")
+        break
