c46454d90c45d6fd5a52204ceb76176ee832862a
shyam9010 <shyamsundar9010@gmail.com>
2024-05-01 00:21:54 +0530

    get the product of the pair

---
+++ max_product_pair.py
@@ -0,0 +1,12 @@
+a = [2, 8, 7, 15]
+
+max = a[0]
+max1 = a[0]
+for i in a:
+    if i > max:
+        max = i
+    for j in a:
+        if j > max1 and max1 < max:
+            max1 = j
+product = max * max1
+print(product)
