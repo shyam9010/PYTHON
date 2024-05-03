691d9853060747744eb17f02ea743de1fca10741
shyam9010 <shyamsundar9010@gmail.com>
2024-05-03 23:36:46 +0530

    find the largest index in the given list using binary search

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,4 @@
-a = [1, 9, 4, 5, 38, 8, 2, 12, 16, 21, 23, 28]
+a = [1, 9, 4, 5, 8, 8, 2, 12, 16, 21, 23, 28]
 
 a.sort()
 
@@ -12,7 +12,7 @@ while start <= end:
     mid = int((start + end) / 2)
     if a[mid] == num:
         print(mid)
-        break
+        start = mid + 1
     elif a[mid] < num:
         start = mid + 1
     else:
