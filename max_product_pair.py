05f93a2156857d75b9244b8dc44a3bd769ee9db4
shyam9010 <shyamsundar9010@gmail.com>
2024-05-12 23:10:44 +0530

    second approach if we have 2 num same

--- max_product_pair.py
+++ max_product_pair.py
@@ -10,3 +10,20 @@ for j in a:
         max1 = j
 product = max * max1
 print(product)
+
+# second approach if we have 2 num same
+
+a = [1, 4, 67, 89]
+max = a[0]
+max1 = a[0]
+ind = 0
+
+for i in range(0, len(a)):
+    if a[i] > max:
+        max = a[i]
+        ind = i
+for j in range(0, len(a)):
+    if a[j] > max1 and a[j] <= max and j != ind:
+        max1 = a[j]
+
+print(max * max1)
