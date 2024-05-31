ff9de1f7b8e51d88ca44cb08339f845a06a93695
shyam9010 <shyamsundar9010@gmail.com>
2024-05-31 20:02:13 +0530

    binary search

--- binary_search.py
+++ binary_search.py
@@ -1,19 +1,20 @@
-a = [1, 9, 4, 5, 8, 8, 2, 12, 16, 21, 23, 28]
+def binary():
+    a = [1, 9, 4, 5, 8, 8, 2, 12, 16, 21, 23, 28]
 
-a.sort()
+    a.sort()
 
-num = int(input(" enter a number : "))
+    num = int(input(" enter a number : "))
 
-start = 0
+    start = 0
 
-end = len(a) - 1
+    end = len(a) - 1
 
-while start <= end:
-    mid = int((start + end) / 2)
-    if a[mid] == num:
-        print(mid)
-        start = mid + 1
-    elif a[mid] < num:
-        start = mid + 1
-    else:
-        end = mid - 1
+    while start <= end:
+        mid = int((start + end) / 2)
+        if a[mid] == num:
+            print(mid)
+            start = mid + 1
+        elif a[mid] < num:
+            start = mid + 1
+        else:
+            end = mid - 1
