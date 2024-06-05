a8350942ae6b6d9bdc1cba5fc63dfd6fbf7e698f
shyam9010 <shyamsundar9010@gmail.com>
2024-06-05 15:50:18 +0530

    rotate list

--- rotate_list.py
+++ rotate_list.py
@@ -9,8 +9,7 @@ r = 3
 list = [1, 2, 3, 4, 5]
 r = r % len(list)
 
-
-reverse(0, r-2, list)
-reverse(r-1, len(list)-1, list)
 reverse(0, len(list)-1, list)
+reverse(0, r-1, list)
+reverse(r, len(list)-1, list)
 print(list)
